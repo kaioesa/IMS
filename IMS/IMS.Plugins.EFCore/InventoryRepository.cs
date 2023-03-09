953fb489c322e795126f07948850d954d812aff8
Kaio Eduardo <xkaio@hotmail.com>
2023-03-09 08:11:01 -0300

    Initial commit

---
+++ IMS/IMS.Plugins.EFCore/InventoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Plugins.EFCore
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly IMSContext _db;
+
+        public InventoryRepository(IMSContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByName(string name)
+        {
+            return await _db.Inventories.Where(x => x.InventoryName.Contains(name) ||
+                                  string.IsNullOrWhiteSpace(name)).ToListAsync();
+        }
+    }
+}
---
+++ IMS/IMS.UseCases/ViewInventoriesByName.cs
@@ -0,0 +1,20 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoriesByName
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public ViewInventoriesByName(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name)
+        {
+            return await _inventoryRepository.GetInventoriesByName(name);
+        }
+    }
+}
